dd251315874e6240c077ee9935140a1274d969fc
Shevdary <Darena080720>
2021-01-22 16:34:45 +0200

    connect with indexedDB

---
+++ src/indexeDB/database.js
@@ -0,0 +1,46 @@
+import Dexie from 'dexie';
+
+const db = new Dexie('myDb');
+db.version(1).stores({
+  user: 'id, name, age',
+});
+
+export const getUsers = () => db.user.toArray();
+
+export const addNewUser = ({
+  username,
+  password,
+  firstName,
+  lastName,
+  gender,
+  birthday,
+  email,
+  address,
+  company,
+  fax,
+  facebookLink,
+  phoneNumber,
+  skills,
+  hobbies,
+}) =>
+  db.user.add({
+    username,
+    password,
+    firstName,
+    lastName,
+    gender,
+    birthday,
+    email,
+    address,
+    company,
+    fax,
+    facebookLink,
+    phoneNumber,
+    skills,
+    hobbies,
+  });
+
+export const updateDataUser = (id, name) => db.user.update(id, { name });
+
+export const deleteUser = (id) => db.user.delete(id);
+export default { db, getUsers, addNewUser, updateDataUser };
