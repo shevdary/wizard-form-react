6191b6c89931c1e42a85cdeddfdda7e12e769877
Shevdary <Darena080720>
2021-01-25 13:49:03 +0200

    update account validate upload file

---
+++ src/components/AccountAvatar/AccountAvatar.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Field } from 'redux-form';
+import { useDispatch } from 'react-redux';
+import { addUserData } from '../../redux/userData/reducers';
+import { AvatarLabel } from '../AccountForm/AccountFormStyled';
+
+const SimpleForm = () => {
+  const dispatch = useDispatch();
+  const [error, setError] = useState(null);
+
+  const onFileChange = (event) => {
+    const image = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.readAsDataURL(image);
+    if (image && image.size / 1024 < 100) {
+      reader.onload = () => {
+        dispatch(addUserData({ avatar: reader.result }));
+      };
+      setError('');
+    }
+
+    if (image && image.size / 1024 > 100) {
+      setError('File must be less than 1Mb');
+    }
+  };
+
+  const renderFileInput = ({ input, type }) => (
+    <div>
+      <input
+        name={input.name}
+        type={type}
+        onChange={onFileChange}
+        id="addAvatar"
+        multiple
+      />
+      <p>{error}</p>
+    </div>
+  );
+
+  return (
+    <AvatarLabel htmlFor="addAvatar">
+      <Field name="image" type="file" component={renderFileInput} />
+      <i>+ addAvatar</i>
+    </AvatarLabel>
+  );
+};
+
+export default SimpleForm;
--- src/components/AccountForm/AccountForm.jsx
+++ src/components/AccountForm/AccountForm.jsx
@@ -13,41 +13,25 @@ import {
   UserAvatar,
   Button,
   ForwardButton,
-  AddAvatar,
   AvatarLabel,
   RequiredField,
   Label,
-  Inputs,
-  SpanError,
 } from './AccountFormStyled';
 import { accountDataValidate } from '../../helpers/accountValidate';
-
-import { useSelector } from 'react-redux';
 import { renderField } from '../../helpers/reduxValidateField';
+import SimpleForm from '../AccountAvatar/AccountAvatar';
 
 const AccountForm = ({ handleSubmit }) => {
   const [visiblePassword, setVisiblePassword] = useState([true, true]);
-  const selector = useSelector(({ form, user }) => ({
-    form: form.userData,
-    user: user.previousStep,
-  }));
-  const [type, setType] = useState('password');
   const [repeatType, setRepeatType] = useState('password');
-  const [file, setFile] = useState(null);
+  const [type, setType] = useState('password');
 
-  const getFile = (event) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(event[0]);
-    reader.onload = (e) => {
-      setFile({ file: reader.result, size: e.loaded });
-    };
-  };
-
-  const changeVisinlePassword = (e) => {
+  const changeVisiblePassword = (e) => {
     e.preventDefault();
     setVisiblePassword([!visiblePassword[0], visiblePassword[1]]);
     !visiblePassword[0] ? setType('password') : setType('text');
   };
+
   const changeVisibleRepeat = (e) => {
     e.preventDefault();
     setVisiblePassword([visiblePassword[0], !visiblePassword[1]]);
@@ -56,70 +40,59 @@ const AccountForm = ({ handleSubmit }) => {
 
   return (
     <>
-      {/*   {selector.user.indexOf('account') && (
-        <Redirect to={'/create-user/profile'} />
-      )}*/}
       <Form className="account">
         <LeftBlock className="left-side">
           <UserAvatar className="avatar">{Avatar}</UserAvatar>
           <AvatarLabel htmlFor="addAvatar">
-            <AddAvatar
-              type="file"
-              multiple
-              onChange={(e) => getFile(e.target.files)}
-              id="addAvatar"
-            />
-            <i>+ addAvatar</i>
+            <SimpleForm id="addAvatar" />
           </AvatarLabel>
         </LeftBlock>
         <RightBlock>
-          <form>
-            <InputForm>
-              <Label htmlFor="username">
-                User name
-                <RequiredField>*</RequiredField>
-              </Label>
-              <Input name="username" type="text" component={renderField} />
-            </InputForm>
-            <InputForm>
-              <Label htmlFor="password">
-                Password
-                <RequiredField>*</RequiredField>
-              </Label>
-              <Input name="password" type={type} component={renderField} />
-              <Button onClick={(e) => changeVisinlePassword(e)}>
-                {visiblePassword[0] ? (
-                  <VisibilityIcon fontSize="small" />
-                ) : (
-                  <VisibilityOffIcon fontSize="small" />
-                )}
-              </Button>
-            </InputForm>
-            <InputForm className="password-wraper">
-              <Label htmlFor="repeatPassword">
-                Repeat password
-                <RequiredField>*</RequiredField>
-              </Label>
-              <Input
-                name="repeatPassword"
-                type={repeatType}
-                component={renderField}
-              />
-              <Button onClick={(e) => changeVisibleRepeat(e)}>
-                {visiblePassword[1] ? (
-                  <VisibilityIcon fontSize="small" />
-                ) : (
-                  <VisibilityOffIcon fontSize="small" />
-                )}
-              </Button>
-            </InputForm>
-            <ForwardButton
-              type="submit"
-              onClick={handleSubmit(accountDataValidate)}
-            >
-              Forward
-            </ForwardButton>
-          </form>
+          <InputForm>
+            <Label htmlFor="username">
+              User name
+              <RequiredField>*</RequiredField>
+            </Label>
+            <Input name="username" type="text" component={renderField} />
+          </InputForm>
+          <InputForm>
+            <Label htmlFor="password">
+              Password
+              <RequiredField>*</RequiredField>
+            </Label>
+            <Input name="password" type={type} component={renderField} />
+            <Button onClick={changeVisiblePassword}>
+              {visiblePassword[0] ? (
+                <VisibilityIcon fontSize="small" />
+              ) : (
+                <VisibilityOffIcon fontSize="small" />
+              )}
+            </Button>
+          </InputForm>
+          <InputForm className="password-wrapper">
+            <Label htmlFor="repeatPassword">
+              Repeat password
+              <RequiredField>*</RequiredField>
+            </Label>
+            <Input
+              name="repeatPassword"
+              type={repeatType}
+              component={renderField}
+            />
+            <Button onClick={changeVisibleRepeat}>
+              {visiblePassword[1] ? (
+                <VisibilityIcon fontSize="small" />
+              ) : (
+                <VisibilityOffIcon fontSize="small" />
+              )}
+            </Button>
+          </InputForm>
+          <ForwardButton
+            type="submit"
+            onClick={handleSubmit(accountDataValidate)}
+          >
+            Forward
+          </ForwardButton>
         </RightBlock>
       </Form>
     </>
