2efc6024e919f9584b57a978b375da8b07c23e25
Shevdary <Darena080720>
2021-01-27 02:30:51 +0200

    setting the address field

--- src/components/ProfileForm/index.jsx
+++ src/components/ProfileForm/index.jsx
@@ -1,117 +1,109 @@
+/*eslint-disable*/
 import React, { useEffect } from 'react';
-import { reduxForm } from 'redux-form';
+import { Field, reduxForm } from 'redux-form';
 import { Redirect, useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 // custom fields
 import Button from '../CustomFields/Button';
-import { redirectStep } from '../../redux/user/reducers';
 import { InputComponent } from '../CustomFields/Input';
-import { DataPickerInput } from '../CustomFields/DataPickerField';
+import { DataPicker } from '../CustomFields/DataPickerField';
 import { RadioSelected } from '../CustomFields/RadioSelected';
 // styled
-import {
-  Form,
-  Input,
-  InputForm,
-  Label,
-  SpanError,
-} from '../AccountForm/styled';
-import { RadioSelect, FlexColumn, Inputs, RightSide, LeftSide } from './styled';
+import { Form, InputForm, Label } from '../AccountForm/styled';
+import { RadioSelect, FlexColumn, RightSide, LeftSide } from './styled';
 import 'react-datepicker/dist/react-datepicker.css';
 // utils
-import { profileDataValidate } from '../../utils/accountValidate';
 
-const renderField = (props) => {
-  const {
-    input,
-    label,
-    type,
-    meta: { touched, error },
-  } = props;
-  return (
-    <InputForm>
-      <Label>{label}</Label>
-      <Inputs {...input} placeholder={label} type={type} />
-      {touched && error && <SpanError>{error}</SpanError>}
-    </InputForm>
-  );
-};
+import { renderField } from '../../utils/reduxValidateField';
+import { profileValidate } from '../../utils/profileValidate';
 
-const Index = ({ handleSubmit }) => {
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import { PlaceAutocomplete } from '../CustomFields/PlaceAutocomplete';
+
+const Index = () => {
   const { nextStep } = useSelector((state) => state.user);
   const dispatch = useDispatch();
+  const values = useSelector((state) => state.form.profile);
   const history = useHistory();
-
-  useEffect(() => {
-    dispatch(redirectStep('0'));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   const handleClick = (e) => {
     e.preventDefault();
     history.push('/create-user/account');
   };
 
+  const handleSubmit = () => {
+    const { firstName, lastName, birthday, address } = values;
+    /*dispatch(redirectToNextStep('profile'));
+    dispatch(redirectStep('2'));
+    dispatch(addUserData({ firstName, lastName, birthday, address }));*/
+  };
+
   if (nextStep && nextStep === '2')
     return <Redirect to="/create-user/contact" />;
 
   return (
-    <form>
-      <Form className="account">
-        <LeftSide>
-          <InputComponent
-            name="firstName"
-            type="text"
-            isRequired
-            label="First name"
-            component={renderField}
-          />
-          <InputComponent
-            label="Last name"
-            name="lastName"
-            type="text"
-            isRequired
-            component={renderField}
-          />
-          <DataPickerInput name="birthday" dateFormat="dd/MM/yyyy" />
-        </LeftSide>
-        <RightSide>
-          <InputComponent
-            label="Email"
-            name="email"
-            isRequired
-            type="email"
-            component={renderField}
-          />
-          <InputForm>
-            <Label htmlFor="address">Address</Label>
-            {/* <GooglePlacesAutocomplete apiKey="" /> */}
-            <Input name="address" type="text" component={renderField} />
-          </InputForm>
-          <InputForm>
-            <Label>Gender</Label>
-            <RadioSelect>
-              <RadioSelected
-                name="gender"
-                options={['male', 'female']}
-                component={renderField}
-              />
-            </RadioSelect>
-          </InputForm>
-          <FlexColumn>
-            <Button
-              type="submit"
-              onClick={handleSubmit(profileDataValidate)}
-              label="Forward"
+    <Form className="profile">
+      <LeftSide>
+        <InputComponent
+          name="firstName"
+          type="text"
+          isRequired
+          label="First name"
+          component={renderField}
+        />
+        <InputComponent
+          label="Last name"
+          name="lastName"
+          type="text"
+          isRequired
+          component={renderField}
+        />
+        <Field
+          name="birthday"
+          dateFormat="dd/MM/yyyy"
+          label="Birth date"
+          type="date"
+          minAge={18}
+          component={DataPicker}
+        />
+      </LeftSide>
+      <RightSide>
+        <InputComponent
+          label="Email"
+          name="email"
+          isRequired
+          type="email"
+          minAge={18}
+          component={renderField}
+        />
+        <Field
+          label="Address"
+          name="address"
+          type="text"
+          component={PlaceAutocomplete}
+        />
+        <InputForm>
+          <Label>Gender</Label>
+          <RadioSelect>
+            <RadioSelected
+              name="gender"
+              options={['male', 'female']}
+              component={renderField}
             />
-            <Button onClick={handleClick} label="Back" name="backForm" />
-          </FlexColumn>
-        </RightSide>
-      </Form>
-    </form>
+          </RadioSelect>
+        </InputForm>
+        <FlexColumn>
+          <Button type="submit" onClick={handleSubmit} label="Forward" />
+          <Button onClick={handleClick} label="Back" name="backForm" />
+        </FlexColumn>
+      </RightSide>
+    </Form>
   );
 };
 
 export default reduxForm({
-  form: 'userProfile',
+  form: 'profile',
+  validate: profileValidate,
+  destroyOnUnmount: false,
+  forceUnregisterOnUnmount: true,
 })(Index);
