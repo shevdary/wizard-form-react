81346c95db2a0329970e29f739331a1398957088
Shevdary <Darena080720>
2021-02-04 15:51:57 +0200

    fix: set value to localStorage

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
+// redux
+import { useDispatch } from 'react-redux';
+import { resetUserValue } from 'redux/user/actions';
 // image
 import addUser from 'assets/icon/user.svg';
 import logo from 'assets/icon/logotype.svg';
@@ -11,12 +14,14 @@ import { HeaderButton, Logo, NavItem, NavPanel } from './styled';
 export const NavBar = () => {
   const location = useLocation().pathname;
   const history = useHistory();
+  const dispatch = useDispatch();
 
   const redirectToUsers = () => {
     history.push('/user-list');
   };
 
   const redirectToCreateUser = () => {
+    dispatch(resetUserValue());
     history.push('/create-user');
   };
 
