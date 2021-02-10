ff63db4ff4b51ce64c286f333bfd80ce815b13cd
Shevdary <Darena080720>
2021-02-11 01:21:33 +0200

    add props for not found page

--- src/pages/NotFoundPage/index.jsx
+++ src/pages/NotFoundPage/index.jsx
@@ -1,42 +1,12 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
-// assets
-import not from 'assets/image/404.png';
+
 // styled
-import {
-  ErrorSub,
-  ErrorTitle,
-  ErrorWrapper,
-  RedirectButton,
-  ButtonWrapper,
-} from './styled';
+import { ErrorTitle, ErrorWrapper } from './styled';
 
-const NotFound = ({ title }) => {
-  const history = useHistory();
-
-  const redirectToCreateUser = () => {
-    history.push('/create-user');
-  };
-
-  const redirectToListOfUser = () => {
-    history.push('/user-list');
-  };
-
-  return (
-    <ErrorWrapper src={not}>
-      <ErrorTitle>404 Page is not found</ErrorTitle>
-
-      <ErrorSub>{title || 'Oooops, you went beyond the possible'}</ErrorSub>
-      <ButtonWrapper>
-        <RedirectButton type="button" onClick={redirectToCreateUser} right>
-          Go to create user
-        </RedirectButton>
-        <RedirectButton type="button" onClick={redirectToListOfUser} left>
-          Go to list of user
-        </RedirectButton>
-      </ButtonWrapper>
-    </ErrorWrapper>
-  );
-};
+const NotFound = ({ title }) => (
+  <ErrorWrapper>
+    <ErrorTitle>{`404 ${title || 'Page not found'}`}</ErrorTitle>
+  </ErrorWrapper>
+);
 
 export default NotFound;
